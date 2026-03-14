38e4b29d162918427ada9f6c9bcdd86db2e7df84
Joe Natter <office@dsp-crowd.com>
2026-03-14 15:08:07 +0100

    remove docker

--- docker/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-proj="processing-examples"
-
-cd $(dirname $0)
-dirDocker="$(pwd)"
-
-docker build -t "$proj" --build-arg user="$USER" --build-arg uid="$(id -u)" --build-arg gid="$(id -g)" .
