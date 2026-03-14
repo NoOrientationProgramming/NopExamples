fb04f41b04559f2422638a741ba283f962342ec9
Joe Natter <office@dsp-crowd.com>
2026-03-14 17:39:08 +0100

    MandelBlockFilling(): Add

---
+++ t02_multithreading-mandelbrot/MandelBlockFilling.h
@@ -0,0 +1,72 @@
+/*
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  File created on 14.03.2026
+
+  Copyright (C) 2026, Johannes Natter
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MANDEL_BLOCK_FILLING_H
+#define MANDEL_BLOCK_FILLING_H
+
+#include "Processing.h"
+
+class MandelBlockFilling : public Processing
+{
+
+public:
+
+	static MandelBlockFilling *create()
+	{
+		return new dNoThrow MandelBlockFilling;
+	}
+
+protected:
+
+	virtual ~MandelBlockFilling() {}
+
+private:
+
+	MandelBlockFilling();
+	MandelBlockFilling(const MandelBlockFilling &) = delete;
+	MandelBlockFilling &operator=(const MandelBlockFilling &) = delete;
+
+	/*
+	 * Naming of functions:  objectVerb()
+	 * Example:              peerAdd()
+	 */
+
+	/* member functions */
+	Success process();
+	void processInfo(char *pBuf, char *pBufEnd);
+
+	/* member variables */
+	//uint32_t mStartMs;
+
+	/* static functions */
+
+	/* static variables */
+
+	/* constants */
+
+};
+
+#endif
+
