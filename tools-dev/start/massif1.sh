4491df92d11bd21236e026baa2b2f9f5ac0ca62a
Joe Natter <office@dsp-crowd.com>
2026-03-25 22:42:07 +0100

    tools: Add massif

---
+++ tools-dev/start/massif1.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+dirTut="t01_tcp-echo-server"
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 t??"
+	exit 1
+fi
+
+inUser="$1"
+shift
+
+echo "User input: $inUser"
+
+if [ -d "$inUser" ]; then
+	dirTut="$inUser"
+else
+	dirTut="$(ls -1 | grep "$inUser" | sort | head -n 1)"
+fi
+
+echo "Starting example: $dirTut"
+
+dHere="$(pwd)"
+dTool="$dHere/$(dirname $0)"
+dTarget="$dTool/../../$dirTut/build-native"
+dRelHereToTarget="$(realpath --relative-to=$dHere $dTarget)"
+dRelTargetToTool="$(realpath --relative-to=$dTarget $dTool)"
+
+echo "Target: $dRelHereToTarget"
+
+cd "${dRelHereToTarget}" && \
+ninja && \
+
+valgrind \
+	--tool=massif \
+./app \
+	$@
+
---
+++ tools-dev/start/massif2.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+dirTut="t01_tcp-echo-server"
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 t??"
+	exit 1
+fi
+
+inUser="$1"
+shift
+
+echo "User input: $inUser"
+
+if [ -d "$inUser" ]; then
+	dirTut="$inUser"
+else
+	dirTut="$(ls -1 | grep "$inUser" | sort | head -n 1)"
+fi
+
+echo "Starting example: $dirTut"
+
+dHere="$(pwd)"
+dTool="$dHere/$(dirname $0)"
+dTarget="$dTool/../../$dirTut/build-native"
+dRelHereToTarget="$(realpath --relative-to=$dHere $dTarget)"
+dRelTargetToTool="$(realpath --relative-to=$dTarget $dTool)"
+
+echo "Target: $dRelHereToTarget"
+
+file="$(ls -1 ${dRelHereToTarget}/massif.out.* | sort | tail -n 1)"
+massif-visualizer "$file" > /dev/null 2>&1 &
+
