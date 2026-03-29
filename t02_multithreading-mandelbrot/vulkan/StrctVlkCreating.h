69475bd32b202f77f122e296f2ad5ce68588d036
Joe Natter <office@dsp-crowd.com>
2026-03-29 12:01:04 +0200

    meson: Add optional vulkan

---
+++ t02_multithreading-mandelbrot/vulkan/StrctVlkCreating.h
@@ -0,0 +1,72 @@
+/*
+  This file is part of the DSP-Crowd project
+  https://www.dsp-crowd.com
+
+  Author(s):
+      - Johannes Natter, office@dsp-crowd.com
+
+  File created on 29.03.2026
+
+  Copyright (C) 2026, Johannes Natter
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef STRCT_VLK_CREATING_H
+#define STRCT_VLK_CREATING_H
+
+#include "Processing.h"
+
+class StrctVlkCreating : public Processing
+{
+
+public:
+
+	static StrctVlkCreating *create()
+	{
+		return new dNoThrow StrctVlkCreating;
+	}
+
+protected:
+
+	virtual ~StrctVlkCreating() {}
+
+private:
+
+	StrctVlkCreating();
+	StrctVlkCreating(const StrctVlkCreating &) = delete;
+	StrctVlkCreating &operator=(const StrctVlkCreating &) = delete;
+
+	/*
+	 * Naming of functions:  objectVerb()
+	 * Example:              peerAdd()
+	 */
+
+	/* member functions */
+	Success process();
+	void processInfo(char *pBuf, char *pBufEnd);
+
+	/* member variables */
+	//uint32_t mStartMs;
+
+	/* static functions */
+
+	/* static variables */
+
+	/* constants */
+
+};
+
+#endif
+
